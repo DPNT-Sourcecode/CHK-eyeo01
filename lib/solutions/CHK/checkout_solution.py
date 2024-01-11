868226e2432605cbadb5621e50b6da9adfa791c5
sbx_user1051 <sbx_user1051@169.254.68.77>
2024-01-11 20:46:20 +0000

    Thu Jan 11 20:46:20 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,19 +12,18 @@ def checkout(skus):
     
     total = 0
     items_count = {item: skus.count(item) for item in set(skus)}
+
+    free_b = items_count['E'] // 2
+    items_count['B'] = max(0, items_count['B'] - free_b)
+    
     for item, offers in special_offers.items():
         for offer_quantity, offer_price in offers:
             while items_count[item] >= offer_quantity:
                 total += offer_price
                 items_count[item] -= offer_quantity
     
-    free_b = items_count['E'] // 2
-    items_count['B'] = max(0, items_count['B'] - free_b)
 
     for item, count in items_count.items():
             total += count * prices[item]
 
     return total
-
-
-

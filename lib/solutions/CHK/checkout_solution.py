7e6667c020a084e816b664d7228a36d03035fc3f
sbx_user1051 <sbx_user1051@169.254.68.77>
2024-01-11 20:21:05 +0000

    Thu Jan 11 20:21:05 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -10,5 +10,16 @@ def checkout(skus):
     if not all(sku in prices for sku in skus):
         return -1
     
+    total = 0
+    for item, price in prices.items():
+        count = skus.count(item)
+        if item in special_offers:
+            offer_count, offer_price = special_offers[item]
+            total += (count // offer_count) * offer_price
+            total += (count % offer_count) * price
+        else:
+            total += count * price
+    
+
 
 

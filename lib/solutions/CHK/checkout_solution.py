3b0dcfcd313511e5c206265e92a6a39a9e0fc1f0
sbx_user1051 <sbx_user1051@169.254.68.77>
2024-01-11 20:29:15 +0000

    Thu Jan 11 20:29:15 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,14 +3,15 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
 
-    special_offers = {'A': (3, 130), 'B': (2, 45)}
+    special_offers = {'A': [(5, 200), (3, 130)], 'B': (2, 45)}
 
     if not all(sku in prices for sku in skus):
         return -1
     
     total = 0
+    
     for item, price in prices.items():
         count = skus.count(item)
         if item in special_offers:
@@ -20,3 +21,4 @@ def checkout(skus):
         else:
             total += count * price
     return total
+
